f115a01c03ff5412c3e3a2e54bbd4e1d1923d146
yuanshen <z20230404@gmail.com>
2024-10-20 11:51:34 +0800

    update

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,22 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vprint(pagetable_t pagetable,int level){
+// there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V)){
+      for(int i = 0;i < level;i++){
+        printf("..");
+        if(i != level - 1) printf(" ");
+      }
+      printf("");
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      
+      vprint((pagetable_t)child,level + 1);
+    }
+  }
+  kfree((void*)pagetable);
+}
